b609abad6367229af84756e1ae2c7d3a4213a56b
zaiste <oh@zaiste.net>
2012-02-11 17:59:28 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coworfing::Application.config.secret_token = 'cac9a8ff6e917204ee6e7706f9f596cbc2fd28e6e056947c9115e391322d91b4ac6845d683cf59f05d89e353af853af8ae727254b3b0abdb737ee2e90a666e09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Coworfing::Application.config.session_store :cookie_store, key: '_coworfing_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Coworfing::Application.config.session_store :active_record_store
