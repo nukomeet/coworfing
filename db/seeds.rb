b139c4ee35046fd1fc2b77cac74b99149eb62b88
zaiste <oh@zaiste.net>
2012-03-27 01:16:27 +0200

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts 'Empty the MongoDB database'
-Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
+#Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
 
 puts 'Setting up userz'
 user = User.create! :name => 'Alban LV', :email => 'albanlv@gmail.com', :password => 'please', :password_confirmation => 'please', :role => 'admin', :confirmed_at => 1.day.ago, :confirmation_sent_at => 2.days.ago
