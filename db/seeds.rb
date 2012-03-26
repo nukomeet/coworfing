804d5b6e497322277e2d3729480ac1f4ac890bc4
zaiste <oh@zaiste.net>
2012-03-27 01:30:57 +0200

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@ puts 'Empty the MongoDB database'
 #Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
 
 puts 'Setting up userz'
-user = User.create! :name => 'Alban LV', :email => 'albanlv@gmail.com', :password => 'please', :password_confirmation => 'please', :role => 'admin', :confirmed_at => 1.day.ago, :confirmation_sent_at => 2.days.ago
+user = User.create! :name => 'Alban LV', :email => 'albanlv@gmail.com', :password => 'please', :password_confirmation => 'please', :role => 'admin', :confirmed_at => 1.day.ago, :confirmation_sent_at => 2.days.ago, :username => 'albanlv'
 puts 'New user created: ' << user.name
 
-user = User.create! :name => 'Zaiste!', :email => 'oh@zaiste.net', :password => 'please', :password_confirmation => 'please', :role => 'admin', :confirmed_at => 1.day.ago, :confirmation_sent_at => 2.days.ago
+user = User.create! :name => 'Zaiste!', :email => 'oh@zaiste.net', :password => 'please', :password_confirmation => 'please', :role => 'admin', :confirmed_at => 1.day.ago, :confirmation_sent_at => 2.days.ago, :username => 'zaiste'
 puts 'New user created: ' << user.name
