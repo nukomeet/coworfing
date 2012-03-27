a3888c7500c6db5b1d1774b126bf94973de9970d
zaiste <oh@zaiste.net>
2012-03-27 10:16:50 +0200

    invitation sending

--- app/controllers/demands_controller.rb
+++ app/controllers/demands_controller.rb
@@ -1,6 +1,6 @@
 class DemandsController < ApplicationController
   load_and_authorize_resource
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, except: [:create]
 
   def index
     @demands = Demand.all 
