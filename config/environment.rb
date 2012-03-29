f2a691f836db8d5d1577bfef95f198c5c60342d3
zaiste <oh@zaiste.net>
2012-03-29 12:15:16 +0200

    wip | email config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Coworfing::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => ENV['SENDER_EMAIL'],
+  :password             => ENV['SENDER_PASS'],
+  :authentication       => 'plain',
+  :enable_starttls_auto => true
+}
