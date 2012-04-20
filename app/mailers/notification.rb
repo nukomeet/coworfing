1fc72dec798b28bb0fa65ba0aa17b4ecfeee87e1
Zaiste λ <oh@zaiste.net>
2012-04-20 18:05:06 +0200

    renamed view template for mailer

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -1,8 +1,8 @@
 class Notification < ActionMailer::Base
   default from: "bonjour@nukomeet.com"
 
-  def notification_request(user)
-    mail to: user.email, subject: "Coworfing Request"
+  def request_notification_email(user)
+    mail to: user.email, subject: "Coworfing Request Pending"
   end
 end
 
