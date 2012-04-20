8f2f97d0e85bd4b8d916ef829efb28d4b85a4efc
Albanlv <albanlv@gmail.com>
2012-04-20 19:28:41 +0200

    dodelidoo

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -2,6 +2,7 @@ class Notification < ActionMailer::Base
   default from: "bonjour@nukomeet.com"
 
   def request_notification_email(user)
+    @place_request.receiver = user
     mail to: user.email, subject: "Coworfing Request Pending"
   end
 end
