16178c8a35e605220a7b413430016adf163bb62f
Zaiste λ <oh@zaiste.net>
2012-04-20 17:53:34 +0200

    mailer user email field adjusted

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -1,6 +1,8 @@
 class Notification < ActionMailer::Base
+  default from: "bonjour@nukomeet.com"
+
   def notification_request(user)
-    mail(:to => user, :subject => "Coworfing Request", :from => "bonjour@nukomeet.com")
+    mail to: user.email, subject: "Coworfing Request"
   end
 end
 
