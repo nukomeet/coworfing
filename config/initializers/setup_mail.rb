7e6896ede65d370aa5a1350745dd068d390290eb
Albanlv <albanlv@gmail.com>
2012-04-21 13:22:00 +0200

    fixed invitation mail link

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,4 +1,4 @@
 require "development_mail_interceptor"
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+ActionMailer::Base.default_url_options[:host] = "coworfing.com"
 ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
