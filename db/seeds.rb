3f02fd00dd0cb77d092212d0b882ddba08d9a28c
Zaiste! λ <oh@zaiste.net>
2012-04-26 00:50:00 +0200

    user bug creation fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,10 @@ Mongoid.master.collections.reject { |c| c.name =~ /^system/ }.each(&:drop)
 puts 'Setting up userz'
 user = User.create! :name => 'Alban LV', :email => 'albanlv@gmail.com', :password => 'please', :password_confirmation => 'please', :role => 'admin', :username => 'albanlv'
 user.skip_confirmation!
+user.save
 puts 'New user created: ' << user.name
 
 user = User.create! :name => 'Zaiste!', :email => 'oh@zaiste.net', :password => 'please', :password_confirmation => 'please', :role => 'admin', :username => 'zaiste'
 user.skip_confirmation!
+user.save
 puts 'New user created: ' << user.name
