f042024664dc06ff7408e2cc4758397634c61017
Albanlv <albanlv@gmail.com>
2012-04-25 16:02:41 +0200

    adjustments made

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
     @place = Place.find(params[:place_id])
     @comment = @place.comments.create!(params[:comment])
     @comment.user = current_user
+    @comment.save
     redirect_to @place, notice: 'Hooray, comment was successfully created.'
   end
 end
