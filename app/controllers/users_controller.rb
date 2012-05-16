bd554ceab6c39da68dda33dd032ffd68ca8c9fac
Albanlv <albanlv@gmail.com>
2012-05-16 11:22:34 +0200

    added json to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,9 @@ class UsersController < ApplicationController
   def index
     @featured = User.accessible_by(current_ability).excludes(username: nil).excludes(photo: nil)
     @users = User.accessible_by(current_ability).excludes(username: nil) - @featured
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
   end
 end
