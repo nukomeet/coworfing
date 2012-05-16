2cc8afa8c9318dcdcfeb4c0835ad19b3608640e3
Albanlv <albanlv@gmail.com>
2012-05-16 11:32:33 +0200

    fix2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @users }
+      format.json { render json: @users + @featured }
     end
   end
 end
