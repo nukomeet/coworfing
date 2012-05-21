e1a51bf78eb3e4288443e420bb1c98eb394fb041
Zaiste! λ <oh@zaiste.net>
2012-05-21 15:46:54 +0200

    cancan for map & user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_filter :authenticate_user!, except: [:index]
+  before_filter :authenticate_user!, except: [:index, :show]
   load_and_authorize_resource
 
   def show
