3ec4987d2cc8ec887c13af7c2488488ebf2294bf
Albanlv <albanlv@gmail.com>
2012-06-02 18:13:45 +0200

    invitation token valid for 2 week

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -91,7 +91,7 @@ Devise.setup do |config|
   # The period the generated invitation token is valid, after
   # this period, the invited resource won't be able to accept the invitation.
   # When invite_for is 0 (the default), the invitation won't expire.
-  config.invite_for = 1.weeks
+  config.invite_for = 2.weeks
   
   # Number of invitations users can send.
   # If invitation_limit is nil, users can send unlimited invitations.
