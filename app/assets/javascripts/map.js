9937fc3f6968d335f2893ca1ab2c5f6666182c21
Pierrick le targa <pierrick.le.targa@gmail.com>
2012-06-29 09:51:03 -0400

    Changes to manage cluster of points

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,41 +1,83 @@
+//this function includes all necessary js files for the application
+function include(file)
+{
+ 
+  var script  = document.createElement('script');
+  script.src  = file;
+  script.type = 'text/javascript';
+  script.defer = true;
+ 
+  document.getElementsByTagName('head').item(0).appendChild(script);
+ 
+}
+ 
+/* include any js files here */
+include('markerclusterer.js');
 
+var markers = [];
+var map = null;
 
+var imageUrl = 'http://chart.apis.google.com/chart?cht=mm&chs=24x32&chco=FFFFFF,008CFF,000000&ext=.png';
+var markerImage = new google.maps.MarkerImage(imageUrl, new google.maps.Size(24, 32));
 
-$(function() { 
+$(function () {
     var user_location = "";
 
     user_location = $.parseJSON(
         $.ajax({
-              type: 'GET',
-              url: '/home/location',
-              dataType: 'json',
-              success: function() { },
-              data: {},
-              async: false
+            type: 'GET',
+            url: '/home/location',
+            dataType: 'json',
+            success: function () { },
+            data: {},
+            async: false
         }).responseText
     );
 
-    $('#mapino').gMap({
-        address: user_location.city + ", " + user_location.country,
-        zoom: 12 
-    });
+    map = new google.maps.Map(document.getElementById('mapino'), {
+            zoom: 3,
+            center: new google.maps.LatLng(45.52527, -73.604043),
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            minZoom: 3
+        });
 
 
-    $.getJSON("/search/list", { limit: 50 }, function(json) {
+    $.getJSON("/search/list", { limit: 50 }, function (json) {
         if (json.length > 0) {
-            for (i=0; i<json.length; i++) {
+            for (i = 0; i < json.length; i++) {
                 var place = json[i];
                 addLocation(place);
             }
+
+            var markerClusterer = new MarkerClusterer(map, markers, {
+                maxZoom: 18,
+                gridSize: 30,
+		style: null
+            });
         }
     });
 
-    function addLocation(place) { 
-        $('#mapino').gMap('addMarker',
-            {
-                latitude: place.coordinates[1],
-                longitude: place.coordinates[0],
-                content: "<strong>" + place.name + "</strong><br/><br/><a href='/places/" + place.id + "' class='btn btn-info'>View details »</a>"
+    function addLocation(place) {
+
+        var latLng = new google.maps.LatLng(place.coordinates[1], place.coordinates[0]);
+        var marker = new google.maps.Marker(
+            { 
+                position: latLng,
+                draggable: true,
+                clickable: true,
+                map: map,
+                title: place.name,
+                icon: markerImage
             });
+
+        var info = new google.maps.InfoWindow({
+            content: "<strong>" + place.name + "</strong><br/><br/><a href='/places/" + place.id + "' class='btn btn-info'>View details »</a>"
+            });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            info.open(map, marker);
+        });
+
+        markers.push(marker);
     }
 });
