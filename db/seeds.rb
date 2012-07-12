509250bf2f1c952a4eea4ffd340747d350fc7031
Zaiste! <oh@zaiste.net>
2012-07-12 17:16:49 +0200

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-puts 'Empty the MongoDB database'
-Mongoid.master.collections.reject { |c| c.name =~ /^system/ }.each(&:drop)
-
-puts 'Setting up userz'
-user = User.create! :name => 'Alban LV', :email => 'albanlv@gmail.com', :password => 'please', :password_confirmation => 'please', :role => 'admin', :username => 'albanlv'
-user.skip_confirmation!
-user.save
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please', :confirmed_at => Time.now.utc, username: 'admin'
 puts 'New user created: ' << user.name
-
-user = User.create! :name => 'Zaiste!', :email => 'oh@zaiste.net', :password => 'please', :password_confirmation => 'please', :role => 'admin', :username => 'zaiste'
-user.skip_confirmation!
-user.save
-puts 'New user created: ' << user.name
+user.add_role :admin
