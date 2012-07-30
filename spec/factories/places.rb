df8c396116db54b87c8ce60663a510d40fcfffad
Zaiste! <oh@zaiste.net>
2012-07-30 21:35:49 +0200

    factory for place

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -1,16 +1,12 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
+require 'faker'
 
 FactoryGirl.define do
-  factory :place do
-    name "MyString"
-    desc "MyText"
-    website "MyString"
-    wifi "MyString"
-    transport "MyString"
-    address_line1 "MyString"
-    address_line2 "MyString"
-    city "MyString"
-    zipcode "MyString"
-    country "MyString"
+  factory :place do |p|
+    p.name { Faker::Lorem.words(7) }
+    p.desc { Faker::Lorem.paragraph }
+    p.address_line1 { Faker::Address.street_address }
+    p.city { Faker::Address.city }
+    p.country "France"
+    p.kind :private
   end
 end
