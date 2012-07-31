c16f178b85e012ff366f0b63ae727d964e1a9e04
Bartłomiej Wiaduch <tukan2can@gmail.com>
2012-07-31 09:33:52 +0200

    comments mods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ActiveRecord::Base
   attr_accessible :content
 
   belongs_to :user
+  belongs_to :place
 
   validates :content, length: { in: 5..500 }, presence: true
+  validates :user, :place, presence: true
 end
