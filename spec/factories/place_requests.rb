2e57f9edb60abcac50042a33db2d26f1227c78e4
Zaiste! <oh@zaiste.net>
2012-07-31 03:47:13 +0200

    basic tests for place request model and controller

--- spec/factories/place_requests.rb
+++ spec/factories/place_requests.rb
@@ -2,8 +2,7 @@
 
 FactoryGirl.define do
   factory :place_request do
-    active false
     requested_on "2012-07-11 16:54:25"
-    body "MyText"
+    body { Faker::Lorem.paragraph(1) }
   end
 end
--- spec/models/place_request_spec.rb
+++ spec/models/place_request_spec.rb
@@ -1,10 +1,23 @@
 require 'spec_helper'
 
 describe PlaceRequest do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid facotry" do
+    FactoryGirl.create(:place_request)
+  end 
 
-  describe "should have 'pending' status by default" do
-    pr = PlaceRequest.new
-    pr.status.should == :pending
+  it "is invalid without a requested_on date" do
+    FactoryGirl.build(:place_request, requested_on: nil).should_not be_valid
+  end
+
+  it "is invalid without a body" do
+    FactoryGirl.build(:place_request, body: nil).should_not be_valid
+  end
+
+  it "is invalid with a body shorter than 5 characters" do
+    FactoryGirl.build(:place_request, body: String.random_alphanum(4) ).should_not be_valid
+  end
+  
+  it "is invalid with a body longer than 500 characters" do
+    FactoryGirl.build(:place_request, body: String.random_alphanum(501) ).should_not be_valid
   end
 end
