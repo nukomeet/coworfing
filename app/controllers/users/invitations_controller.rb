3ba001b0284710b54bb72c44457de76b6bc6db01
Tomasz Kuklis <tomasz.kuklis@gmail.com>
2012-08-08 14:29:13 +0200

    cleaned invitations

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -6,28 +6,13 @@ class Users::InvitationsController < Devise::InvitationsController
     super
   end
    
-   def create
+  def create
     authorize! :invite, User
     super
-   end
-
-   # PUT /resource/invitation
-  def update
-    self.resource = resource_class.accept_invitation!(params[resource_name])
-
-    resource.role = "regular"
-
-    if resource.errors.empty?
-      set_flash_message :notice, :updated
-      sign_in(resource_name, resource)
-      respond_with resource, :location => after_accept_path_for(resource)
-    else
-      respond_with_navigational(resource){ render :edit }
-    end
   end
 
   def after_invite_path_for(resource)
-    root_path(locale: 'en')
+    root_path
   end
 
 end
