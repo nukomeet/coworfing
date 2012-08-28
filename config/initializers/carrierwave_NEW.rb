d7db69cca20940273ad629d7ec9d801936c3cf97
Zaiste! <oh@zaiste.net>
2012-08-28 10:00:00 +0200

    brand new carrierwave initializer with hidden credentials

---
+++ config/initializers/carrierwave_NEW.rb
@@ -0,0 +1,16 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
+    config.fog_credentials = {
+      :provider               => 'AWS',       # required
+      :aws_access_key_id      => ENV['S3_KEY'],       # required
+      :aws_secret_access_key  => ENV['S3_SECRET'],       # required
+      :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+    }
+    config.fog_directory  = 'coworfing-production'
+    config.fog_public = false
+    config.storage = :fog
+  else
+    config.storage = :file
+  end
+end
