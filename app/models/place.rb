400748159102a54cf30e24781165a578a1390fd7
Tomasz Kuklis <tomasz.kuklis@gmail.com>
2012-10-09 13:17:16 +0200

    photos migration

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ActiveRecord::Base
-
+  mount_uploader :photo, PhotoUploader
+  
   acts_as_taggable
   attr_accessible :address_line1, :address_line2, :city, :country, :desc, :name, :transport, :website, :wifi, :zipcode, :kind, :features, :photos_attributes, :tag_list
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120926112536) do
+ActiveRecord::Schema.define(:version => 20120924101654) do
 
   create_table "comments", :force => true do |t|
     t.text     "content"
@@ -19,13 +19,6 @@ ActiveRecord::Schema.define(:version => 20120926112536) do
     t.datetime "updated_at", :null => false
     t.integer  "place_id"
     t.integer  "user_id"
-  end
-
-  create_table "photos", :force => true do |t|
-    t.integer  "place_id"
-    t.string   "photo"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
   create_table "place_requests", :force => true do |t|
@@ -57,23 +50,7 @@ ActiveRecord::Schema.define(:version => 20120926112536) do
     t.integer  "user_id"
     t.string   "kind"
     t.integer  "features"
-  end
-
-  create_table "taggings", :force => true do |t|
-    t.integer  "tag_id"
-    t.integer  "taggable_id"
-    t.string   "taggable_type"
-    t.integer  "tagger_id"
-    t.string   "tagger_type"
-    t.string   "context",       :limit => 128
-    t.datetime "created_at"
-  end
-
-  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
-
-  create_table "tags", :force => true do |t|
-    t.string "name"
+    t.string   "photo"
   end
 
   create_table "users", :force => true do |t|
