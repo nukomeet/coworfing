74f18009aa47a8eb5d88c3659bbaf3ae0b5eae71
Zaiste! <oh@zaiste.net>
2012-11-23 19:19:07 +0100

    tests rewritten

--- spec/controllers/checkins_controller_spec.rb
+++ spec/controllers/checkins_controller_spec.rb
@@ -2,25 +2,4 @@ require 'spec_helper'
 
 describe CheckinsController do
 
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'update'" do
-    it "returns http success" do
-      get 'update'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'delete'" do
-    it "returns http success" do
-      get 'delete'
-      response.should be_success
-    end
-  end
-
 end
