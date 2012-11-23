bc3b325b4bd699e3b04457117dbf2326ba82a866
Zaiste! <oh@zaiste.net>
2012-11-23 19:14:05 +0100

    no spaces

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,28 +32,28 @@ module ApplicationHelper
   end
 
   def confirm_button_with_class(name, path, confirm_text, classes, disabled=false, method=:get)
-    if disabled 
+    if disabled
       link_to(name, '#', class: classes + ' disabled')
     else
       link_to(name, path, data: { confirm: confirm_text }, class: classes, method: method)
     end
   end
-  
+
   def invitation_status(user)
-    user.invitation_accepted? ? "accepted" : "awaiting"   
+    user.invitation_accepted? ? "accepted" : "awaiting"
   end
 
-  def avatar_url(email, options = {})  
+  def avatar_url(email, options = {})
     width = options[:width] || 210
     default = options[:default] || "mm"
-    gravatar_id = Digest::MD5::hexdigest(email).downcase  
-    "http://gravatar.com/avatar/#{gravatar_id}?s=#{width}&d=#{default}"  
-  end  
+    gravatar_id = Digest::MD5::hexdigest(email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}?s=#{width}&d=#{default}"
+  end
 
   def url_with_protocol(url)
     /^http/.match(url) ? url : "http://#{url}"
   end
-  
+
   def link_to_add_fields(name, f, association)
     new_object = f.object.send(association).klass.new
     id = new_object.object_id
