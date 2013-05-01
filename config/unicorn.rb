225bc9e127f0e84af4b8666e243a9861ff136f71
Zaiste! <oh@zaiste.net>
2013-05-01 15:27:35 -0700

    Updated `unicorn.rb` and dependencies.
    Adjusted `unicorn.rb` so it handles two sockets. Updated gems to
    newest versions. Removed deploy script.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,23 @@
-root = "/home/deployer/apps/coworfing/current"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+worker_processes 4
+working_directory "/home/deployer/apps/coworfing/current"
 
-listen "/tmp/unicorn.coworfing.sock"
-worker_processes 2
+listen "/tmp/coworfing.unicorn.sock.0", :backlog => 64
+listen "/tmp/coworfing.unicorn.sock.1", :backlog => 64
+
 timeout 30
+
+pid "/home/deployer/apps/coworfing/tmp/unicorn.pid"
+
+stderr_path "/home/deployer/apps/coworfing/current/log/unicorn.stderr.log"
+stdout_path "/home/deployer/apps/coworfing/current/log/unicorn.stdout.log"
+
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
