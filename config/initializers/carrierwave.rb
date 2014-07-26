8e98384ce473c07d280150da3dd5b46c70572f26
tomkuk <tomasz.kuklis@gmail.com>
2014-07-26 19:36:13 +0200

    fix: aws bucket
    move bucket directory to env
    
    [0:10]

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
       :aws_secret_access_key  => ENV['S3_SECRET'],       # required
       :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
     }
-    config.fog_directory  = 'coworfing-production'
+    config.fog_directory  = ENV['S3_BUCKET']
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
     config.fog_public = false
     config.storage = :fog
